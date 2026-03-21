fb65e1ffd8d821e8555800ec0f8030f9b377467c
arthurdehaye <arthurdehaye@gmail.com>
2026-03-21 22:28:14 +0100

    Fixed Player cooldowns

--- Script/Player/Player.gd
+++ Script/Player/Player.gd
@@ -18,12 +18,12 @@ var ennemie : Monster
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	actions.append(Spell.new(100,0,0,true,Action.Change.NONE,Action.Effect.DMG,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
-	actions.append(Spell.new(250,2,0,false,Action.Change.DAY,Action.Effect.DMG,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
-	actions.append(Spell.new(150,1,0,false,Action.Change.DAY,Action.Effect.DMG,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
+	actions.append(Spell.new(250,3,0,false,Action.Change.DAY,Action.Effect.DMG,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
+	actions.append(Spell.new(150,2,0,false,Action.Change.DAY,Action.Effect.DMG,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
 	#Multiplicateur de degat (prendre en pourcentage)
-	actions.append(Spell.new(75,1,0,true,Action.Change.NIGHT,Action.Effect.INFLICT,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
+	actions.append(Spell.new(75,2,0,true,Action.Change.NIGHT,Action.Effect.INFLICT,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
 	#Reduction de degat (prendre en pourcentage)
-	actions.append(Spell.new(50,1,0,true,Action.Change.NIGHT,Action.Effect.RECEIVE,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
+	actions.append(Spell.new(50,2,0,true,Action.Change.NIGHT,Action.Effect.RECEIVE,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
 	interface.start(hpmax,ennemie,actions)
 
 
