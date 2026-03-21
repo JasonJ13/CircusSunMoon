db01b9d4c01a6b21655dbc6fad1bf3560ab741cb
lolobanius <lubin.hideux@gmail.com>
2026-03-21 10:20:14 +0100

    architecture

---
+++ Script/Player.gd
@@ -0,0 +1,10 @@
+extends Control
+
+var hp: int = 100
+var playerTurn: bool = false
+
+@onready var interface: Control = $PlayerInterface
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
