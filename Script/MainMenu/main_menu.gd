f518c38346297e657b888af9e43cf0ff4e99a277
Kaily <41388029+idimension18@users.noreply.github.com>
2026-03-21 20:00:10 +0100

    fixed broken play button

--- Script/MainMenu/main_menu.gd
+++ Script/MainMenu/main_menu.gd
@@ -17,3 +17,8 @@ func _on_quit_button_pressed() -> void:
 func _on_play_button_pressed() -> void:
 	visible = false
 	on_play.emit()
+
+
+func _on_play_buton_pressed() -> void:
+	visible = false
+	on_play.emit()
