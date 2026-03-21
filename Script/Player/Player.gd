a5bc6c84bcef522a503b9a00cc052d167ae5741e
lolobanius <lubin.hideux@gmail.com>
2026-03-21 21:56:50 +0100

    dbt action player

--- Script/Player/Player.gd
+++ Script/Player/Player.gd
@@ -6,7 +6,8 @@ var playerTurn: bool = false
 @onready var interface: PlayerInterface = $PlayerInterface
 const hpmax: int = 1000
 var actions: Array[Spell] = [] #Toutes les actions possibles
-var dmgModifier: float = 1
+var dmgInflictModifier: float = 1
+var dmgReceiveModifier: float = 1
 
 var ennemies : Array[Monster] = []
 
@@ -14,25 +15,27 @@ var ennemies : Array[Monster] = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	actions.append(Spell.new(100,0,0,true,Action.Change.NONE,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
-	actions.append(Spell.new(250,2,0,false,Action.Change.DAY,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
-	actions.append(Spell.new(150,1,0,false,Action.Change.DAY,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
+	actions.append(Spell.new(100,0,0,true,Action.Change.NONE,Action.Effect.DMG,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
+	actions.append(Spell.new(250,2,0,false,Action.Change.DAY,Action.Effect.DMG,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
+	actions.append(Spell.new(150,1,0,false,Action.Change.DAY,Action.Effect.DMG,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
 	#Multiplicateur de degat (prendre en pourcentage)
-	actions.append(Spell.new(75,1,0,true,Action.Change.NIGHT,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
+	actions.append(Spell.new(75,1,0,true,Action.Change.NIGHT,Action.Effect.INFLICT,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
 	#Reduction de degat (prendre en pourcentage)
-	actions.append(Spell.new(50,1,0,true,Action.Change.NIGHT,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
+	actions.append(Spell.new(50,1,0,true,Action.Change.NIGHT,Action.Effect.RECEIVE,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
 	interface.start(hpmax,ennemies,actions)
 
 
 func takeAction(ennemies: Array[Monster]) -> Action:
 	interface.start(hp, ennemies, actions)
 	var x = await interface.action
-	var res: Action = actions[x[0]]
-	if x[1] == 0:
-		res.cible= Action.Cible.PLAYER
-	else:	
-		res.cible = Action.Cible.MONSTER
-	return res
+	if x.effect == Action.Effect.INFLICT:
+		dmgInflictModifier = 0.75
+		x.dmg = 0
+	elif x.effect == Action.Effect.RECEIVE:
+		dmgReceiveModifier = 50
+		x.dmg = 0
+	
+	return x
 
 func dayTime() -> void :
 	sprite.play("Day")
