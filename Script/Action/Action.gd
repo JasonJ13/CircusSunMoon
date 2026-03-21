d21ca33d356f5047171c21bffce6f561bb2fc60c
lolobanius <lubin.hideux@gmail.com>
2026-03-21 11:53:36 +0100

    jsp

--- Script/Action/Action.gd
+++ Script/Action/Action.gd
@@ -1,10 +1,12 @@
 class_name Action
 
 extends Node
+enum Change {DAY, NIGHT, NONE}
 
 var dmg: int
 var cible: Control
 var cooldown: int = 0
+var change: Change = Change.NONE
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
