e4c5959f2dc13ef1c77cb19c0c4a19071babf1c5
arthurdehaye <arthurdehaye@gmail.com>
2026-03-21 21:40:28 +0100

    Added NightMonster script

--- Script/Monster/NightMonster.gd
+++ Script/Monster/NightMonster.gd
@@ -1,24 +1,24 @@
 extends Monster
-class_name Ballon
+class_name NightMonster
 
 func _init() -> void:
 	hp = 1000
-	dmg = 100
-	nom = "Ballon"
-	var action1 : Action = Action.new(-150, 3, 0, true, Action.Change.NONE)
-	action1.cible = Action.Cible.MONSTER
+	dmg = 75
+	nom = "NightMonster"
+	var action1: Action = Action.new(225, 3, 0, false, Action.Change.NONE)
+	action1.cible = Action.Cible.PLAYER
 	actions = [action1]
 	
 func pass_jour() -> void:
-	dmg = 100
-	
+	dmg=75
+
 func pass_nuit() -> void:
-	dmg=50
+	dmg=150
 	
 func takeAction(day:bool) -> Action:
 	if actions[0].turnsBeforeUse == 0:
 		return actions[0]
 	else:
 		var action = Action.new(dmg, 0, 0, true, Action.Change.NONE)
-		action.cible = Action.Cible.PLAYER
+		action.cible=Action.Cible.PLAYER
 		return action
