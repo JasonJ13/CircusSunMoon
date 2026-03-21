2b5915e2db919ecefd90eee907711810de068ca6
JasonJ13 <jason.jallabert@orange.fr>
2026-03-21 18:33:54 +0100

    sprite player

--- Script/Player/Player.gd
+++ Script/Player/Player.gd
@@ -7,6 +7,8 @@ var playerTurn: bool = false
 const hpmax: int = 100
 var actions: Array[Action] = [] #Toutes les actions possibles
 
+@onready var sprite : AnimatedSprite2D = $SpritePlayer/Sprite
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	actions.append(Action.new(100,0,0,true,Action.Change.NONE))
@@ -17,3 +19,10 @@ func _ready() -> void:
 func takeAction(ennemies: Array[Monster]) -> Action:
 	#à faire: Envoyer les ennemies, les pvs du joueur et ses actions à PlayerInterface
 	return null
+
+
+func dayTime() -> void :
+	sprite.play("Day")
+
+func nightTime() -> void :
+	sprite.play("Night")
