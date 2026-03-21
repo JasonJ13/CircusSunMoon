8b3331a8d6fa63319a8931f532437cefa3ff5109
lolobanius <lubin.hideux@gmail.com>
2026-03-21 21:59:10 +0100

    Update Player.gd

--- Script/Player/Player.gd
+++ Script/Player/Player.gd
@@ -9,7 +9,7 @@ var actions: Array[Spell] = [] #Toutes les actions possibles
 var dmgInflictModifier: float = 1
 var dmgReceiveModifier: float = 1
 
-var ennemies : Array[Monster] = []
+var ennemie : Monster
 
 @onready var sprite : AnimatedSprite2D = $SpritePlayer/Sprite
 
@@ -22,11 +22,11 @@ func _ready() -> void:
 	actions.append(Spell.new(75,1,0,true,Action.Change.NIGHT,Action.Effect.INFLICT,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
 	#Reduction de degat (prendre en pourcentage)
 	actions.append(Spell.new(50,1,0,true,Action.Change.NIGHT,Action.Effect.RECEIVE,preload("res://Asset/other/bouton spell.png"), preload("res://Asset/other/bouton spell hovered.png")))
-	interface.start(hpmax,ennemies,actions)
+	interface.start(hpmax,ennemie,actions)
 
 
-func takeAction(ennemies: Array[Monster]) -> Action:
-	interface.start(hp, ennemies, actions)
+func takeAction(ennemie : Monster) -> Action:
+	interface.start(hp, ennemie, actions)
 	var x = await interface.action
 	if x.effect == Action.Effect.INFLICT:
 		dmgInflictModifier = 0.75
