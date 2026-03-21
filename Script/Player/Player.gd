e941eb0858663e17fe6cf826f9b13467e22d63ca
arthurdehaye <arthurdehaye@gmail.com>
2026-03-21 14:28:31 +0100

    Update Player.gd

--- Script/Player/Player.gd
+++ Script/Player/Player.gd
@@ -3,13 +3,15 @@ class_name Player
 
 var hp: int = 100
 var playerTurn: bool = false
-var decision: Action
 @onready var interface: Control = $PlayerInterface
 const hpmax: int = 100
+var actions: Array[Action] #Toutes les actions possibles
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-	
-func takeAction() -> Action:
+	#à faire: initialiser les actions
+	pass
+		
+func takeAction(ennemies: Array[Monster]) -> Action:
+	#à faire: Envoyer les ennemies, les pvs du joueur et ses actions à PlayerInterface
 	return null
