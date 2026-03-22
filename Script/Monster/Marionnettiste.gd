707703607b307e844333ff45d8100d83bfcd1a9c
arthurdehaye <arthurdehaye@gmail.com>
2026-03-22 10:17:13 +0100

    Added Marionnettiste

--- Script/Monster/Marionnettiste.gd
+++ Script/Monster/Marionnettiste.gd
@@ -1,21 +1,25 @@
 extends Monster
 class_name Marionnettiste
 
-var state = false 	#Etat du marionnettiste (jour=true ; nuit=false)
+var state: bool = false 	#Etat du marionnettiste (jour=true ; nuit=false)
+var actionsDay: Array[Action]	#Sorts utilisables quand le marionnettiste est dans l'état jour
+var actionsNight: Array[Action] #Sorts utilisables quand le marionnettiste est dans l'état jour
 
 func _init() -> void:
 	hp = 1000
 	dmg = 75
 	nom = "Marionnettiste"
-	var actionNuit1: Action = Action.new(225, 2, 0, false, Action.Change.NONE, Action.Effect.DMG)
-	actionNuit1.cible = Action.Cible.PLAYER
-	var actionNuit2: Action = Action.new(100, 2, 2, true, Action.Change.NONE, Action.Effect.DMG)
-	actionNuit2.cible = Action.Cible.PLAYER
-	var actionJour1: Action = Action.new(-150, 3, 0, true, Action.Change.NONE, Action.Effect.DMG)
-	actionJour1.cible = Action.Cible.MONSTER
-	var actionJour2: Action = Action.new(150, 2, 2, false, Action.Change.NONE, Action.Effect.DMG)
-	actionJour2.cible = Action.Cible.PLAYER
-	actions = [actionNuit1, actionNuit2, actionJour1, actionJour2]
+	var actionNight1: Action = Action.new(225, 2, 0, false, Action.Change.NONE, Action.Effect.DMG)
+	actionNight1.cible = Action.Cible.PLAYER
+	var actionNight2: Action = Action.new(100, 2, 2, true, Action.Change.NONE, Action.Effect.DMG)
+	actionNight2.cible = Action.Cible.PLAYER
+	actionsNight = [actionNight1, actionNight2]
+	var actionDay1: Action = Action.new(-150, 3, 0, true, Action.Change.NONE, Action.Effect.DMG)
+	actionDay1.cible = Action.Cible.MONSTER
+	var actionDay2: Action = Action.new(150, 2, 2, false, Action.Change.NONE, Action.Effect.DMG)
+	actionDay2.cible = Action.Cible.PLAYER
+	actionsDay = [actionDay1, actionDay2]
+	actions = actionsNight
 	
 func pass_jour() -> void:
 	if state:
@@ -29,35 +33,23 @@ func pass_nuit() -> void:
 	else:
 		dmg=150
 	
-func takeAction(day:bool) -> Action:
-	#En mode jour
-	if state:
-		#Action de changement d'état
-		if actions[3].turnsBeforeUse==0 and not day:
-			state=false
-			dmg = 150
-			return actions[3]
-		#Autre action
-		elif actions[2].turnsBeforeUse==0:
-			return actions[2]
-		#Attaque de base
-		else:
-			var action = Action.new(dmg, 0, 0, true, Action.Change.NONE, Action.Effect.DMG)
-			action.cible=Action.Cible.PLAYER
-			return action
-			
-	#En mode nuit
-	else:
-		#Action de changement d'état
-		if actions[1].turnsBeforeUse==0 and day:
-			state=true
+func takeAction(isday:bool) -> Action:
+	#action de changement d'état
+	if actions[1].turnsBeforeUse <= 0 and state!=isday:
+		state = not state
+		if state:
 			dmg = 100
-			return actions[1]
-		#Autre action
-		elif actions[0].turnsBeforeUse==0:
-			return actions[0]
-		#Attaque de base
+			actions = actionsDay
+			return actionsNight[1]
 		else:
-			var action = Action.new(dmg, 0, 0, true, Action.Change.NONE, Action.Effect.DMG)
-			action.cible=Action.Cible.PLAYER
-			return action
+			dmg = 150
+			actions = actionsNight
+			return actionsDay[1]
+	#autre action
+	elif actions[0].turnsBeforeUse <= 0:
+		return actions[0]
+	#attaque de base
+	else:
+		var action = Action.new(dmg, 0, 0, true, Action.Change.NONE, Action.Effect.DMG)
+		action.cible=Action.Cible.PLAYER
+		return action
